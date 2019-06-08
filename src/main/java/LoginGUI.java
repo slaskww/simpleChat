24bc8628202f9862b2e0e01507f45c0bd1c62bc3
lukasz <slaskww@gmail.com>
2019-06-08 14:12:00 +0200

    Edit class LoginGUI

--- src/main/java/LoginGUI.java
+++ src/main/java/LoginGUI.java
@@ -10,12 +10,20 @@ public class LoginGUI extends JFrame {
 
     private JPanel contentPane; //nasz panel logowania
     private JTextField txtName; //pole przyjmujące nazwe usera
-    JLabel lblName;
+    private JLabel lblName;
+    private JTextField txtIPAddress;
+    private JLabel lblIPAddress;
+    private JLabel lblIPAddressHint;
+    private JTextField txtPort;
+    private JLabel lblPort;
+    private JLabel lblPortHint;
+
+    private JButton btnLogin;
 
     public LoginGUI() { //konstruktor ustawia wartosci domyslne
         setTitle("Login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 380); //frame size
+        setSize(300, 400); //frame size
         setLocationRelativeTo(null); // If the component is null , the window is placed in the center of the screen
         contentPane = new JPanel(); //na panelu umiejscowiamy buttony i labele
         contentPane.setBorder(new EmptyBorder(5,5,5,5));
@@ -25,13 +33,40 @@ public class LoginGUI extends JFrame {
 
 
         txtName = new JTextField();
-        txtName.setBounds(67,50,165,28);
+        txtName.setBounds(70,50,160,33);
         contentPane.add(txtName);
      //   txtName.setColumns(10);
-
         lblName = new JLabel("Name:");
-        lblName.setBounds(127, 34, 45, 16);
+        lblName.setBounds(125, 32, 40, 18);
         contentPane.add(lblName);
+
+        txtIPAddress = new JTextField();
+        txtIPAddress.setBounds(70,126,160,33);
+        contentPane.add(txtIPAddress);
+        //   txtName.setColumns(10);
+        lblIPAddress = new JLabel("IP Address:");
+        lblIPAddress.setBounds(112, 108, 80, 18);
+        contentPane.add(lblIPAddress);
+        txtIPAddress.setColumns(10);
+        lblIPAddressHint = new JLabel("(ex. 192.168.0.1)");
+        lblIPAddressHint.setBounds(85, 160,120, 16 );
+        lblIPAddressHint.setHorizontalAlignment(SwingConstants.CENTER);
+        contentPane.add(lblIPAddressHint);
+
+        txtPort = new JTextField();
+        txtPort.setBounds(70,210,160,33);
+        contentPane.add(txtPort);
+        lblPort = new JLabel("Port:");
+        lblPort.setBounds(129, 192, 80, 18);
+        contentPane.add(lblPort);
+        lblPortHint = new JLabel(("(ex. 1234)"));
+        lblPortHint.setBounds(120, 240, 80, 18);
+        contentPane.add(lblPortHint);
+
+        btnLogin = new JButton("Login");
+        btnLogin.setBounds(105, 300, 80, 33);
+        contentPane.add(btnLogin);
+
     }
 
     public static void main(String[] args) {
